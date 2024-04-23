bbe3529ecd33d07770e52306e6418c807be7ce85
isimbinadege <isimbi0123@gmail.com>
2024-04-23 08:20:07 -0700

    first commit

---
+++ src/components/navs/navs.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className=" bg-black">
+        <div className="bg-black text-white px- flex flex-wrap w-full"> 
+        <img src="Group 1.png" className="ml-12 py-6"></img>
+        <nav  className="ml-96">
+           <ul className="flex flex-wrap py-8">
+            <li className="px-8 font-semibold">Home</li>
+            <li className="px-8">Genre</li>
+            <li className="px-8">Explore</li>
+            <li className="px-8">News</li>
+            <li className="px-8">Movies</li>
+            <li>Tv shows</li>
+              </ul> 
+        </nav>
+        </div>
+        <div className="grid grid-cols-2">
+            <div className="ml-12 mt-60">
+                <h1 className="text-white font-bold text-3xl">FIND MOVIES</h1>
+                <h1 className="text-6xl  font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-red-700">TV SHOWS AND MORE</h1>
+                <p className="text-white py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt animi porro aliquid ad, rerum esse alias hic dolor architecto possimus eius illum nisi? Exercitationem, tempore. Maxime nemo veritatis eum.</p>
+              <button className="border-white text-white border-2 w-36">watch tutorial</button>
+            </div>
+            <div className=" flex flex-wrap">
+                <img src="img (2).png" className=" ml-96 mt-20 relative" />
+                <img src="img (1).png" alt="" className=" right-60 mt-36 absolute" />
+            </div>
+            <div className="flex flex-wrap mt-36">
+            <h1 className="text-white ml-12  font-bold text-2xl">Trending</h1><hr ></hr>
+            <h1 className="ml-auto text-white text-2xl">See more</h1>
+            </div></div>
+            <div className="grid grid-cols-6 mt-14 text-white">
+          <div>
+            <img src="img (2).png" className="ml-12"></img>
+            <p className="ml-12 text-2xl font-bold">Guardian of the...</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Group (1).png " className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">Shazam fury.</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Group (2).png" className="ml-12"></img>
+            <p className="ml-12 text-xl font-bold">Dungeons&Drag</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector (1).png" className="ml-12"></img>
+            <p className="ml-12 font-bold">Medellin</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector (2).png" className="ml-12"></img>
+            <p className="ml-12">John wick:chapt</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="img (1).png " className="ml-12"></img>
+            <p className="ml-12">Spider-Man:acro..</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+        </div>
+       
+        // This is a JavaScript-style comment
+
+        
+        <div className="grid grid-cols-6 mt-10 text-white">
+          <div>
+            <img src="Vector 12.png" className="ml-12"></img>
+            <p className="ml-12">Tyler perry's sist</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector11.png" className="ml-12"></img>
+            <p className="ml-12">The Cube</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            
+            
+          <div>
+            <img src="Group (14).png" className="ml-12"></img>
+            <p className="ml-12">The Cube</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="bb.png" className="ml-12"></img>
+            <p className="ml-12">Rich in Love</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="bc.png" className="ml-12"></img>
+            <p className="ml-12">The Black Demon</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="Vector (10).png" className="ml-12"></img>
+            <p className="ml-12">The Prank Panel</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            </div>
+            //SECOND PARA
+            <div className="flex flex-wrap mt-36">
+            <h1 className="text-white ml-12  font-bold text-2xl">YOU MAY LIKE THIS</h1><hr ></hr>
+            <h1 className="ml-auto text-white text-m">See more</h1>
+            </div>
+            <div className="grid grid-cols-6  mt-14 text-white">
+          <div>
+            <img src="Vector (5).png" className="ml-12"></img>
+            <p className="ml-12 text-2xl font-bold">Hypnotic</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="moja.png " className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">Mojave Diamonds</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="moj.png " className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">Mojave.</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector (6).png " className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">CrossFire.</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector (7).png" className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">Fast x.</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Vector (8).png " className="ml-12"></img>
+            
+            <p className="ml-12  text-2xl font-bold">The pregnancy.</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            </div>
+            //second
+            <div className="grid grid-cols-6 mt-10 text-white">
+          <div>
+            <img src="Vector (9).png" className="ml-12"></img>
+            <p className="ml-12">THE DAYS</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            <div>
+            <img src="Group (4).png" className="ml-12"></img>
+            <p className="ml-12">Days of Daisy</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            
+            
+          <div>
+            <img src="Group (5).png" className="ml-12"></img>
+            <p className="ml-12">TURN of TIDE</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="Vector (10).png" className="ml-12"></img>
+            <p className="ml-12">Thw Prank Panel</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="Vector (11).png" className="ml-12"></img>
+            <p className="ml-12">The Idol</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+          <div>
+            <img src="Vector (12).png" className="ml-12"></img>
+            <p className="ml-12">The GRYPHON</p>
+            <p className="ml-12">2023       104m</p>
+            </div>
+            </div>
+        </div>
+    )
+}
